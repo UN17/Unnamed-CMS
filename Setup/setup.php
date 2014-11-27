e2149c0f786762e9939b5a3dda05d92fe64704aa
Tarik Onalan <phi.quantum@gmail.com>
2014-11-27 13:12:20 -0800

    Require dependencies

--- Setup/setup.php
+++ Setup/setup.php
@@ -4,9 +4,7 @@
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
 
-<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+<script src="../bower_components/requirejs/require.js"></script>
 
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
---
+++ js/palette.js
@@ -0,0 +1,6 @@
+require.config({
+    path: {
+        jquery: "//code.jquery.com/jquery-1.11.0.min.js"
+        bootstrap: "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"
+    }
+})
